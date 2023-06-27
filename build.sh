f6a36b9110cf6389b1086cf5b025919d455b5622
Vincent Knecht <vincent.knecht@mailoo.org>
2023-06-27 18:11:07 +0200

    build.sh: Support optional device name
    Add support for an optional argument to select device.
    Use idol347 as default device.
    
    Signed-off-by: Vincent Knecht <vincent.knecht@mailoo.org>

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-echo Building the Android
+TARGETDEV=$1
+
+if [ -z "$TARGETDEV" ] ; then
+	TARGETDEV=idol347
+fi
+
+if [ ! -d devices/"$TARGETDEV" ] ; then
+	echo Unknow device "$TARGETDEV"
+	exit 1
+fi
+
+echo Building the Android for "$TARGETDEV"
 pushd aosptree
 . build/envsetup.sh
-lunch idol347-userdebug
-#lunch kiwi-userdebug
+lunch "$TARGETDEV"-userdebug
 
 make -j15 || make -j1
 # Unknown targets after disabling BOARD_USES_GLODROID_MAKE_TARGETS
